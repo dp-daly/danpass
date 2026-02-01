ace400e91ccd11e959e998e2e26dfc3cf4ab3d13
dp-daly <danpatdaly@gmail.com>
2026-02-01 22:30:51 +0000

    Config Rspec and Devise

---
+++ spec/rails_helper.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../config/environment', __dir__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'support/factory_bot'
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.use_transactional_fixtures = true
+
+  config.before(:each, type: :system) do
+    if ENV['CI']
+      driven_by(:selenium, using: :headless_chrome)
+    else
+      driven_by(:rack_test)
+    end
+  end
+end
